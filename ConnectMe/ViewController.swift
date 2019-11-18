89109aa4ff1acdec72165c7f8ca3f04191024972
jerry1ye10 <jerry1ye10@gmail.com>
2019-11-18 14:57:39 -0500

    added initial project connected to firebase

---
+++ ConnectMe/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ConnectMe
+//
+//  Created by Jerry Ye on 11/18/19.
+//  Copyright © 2019 Jerry Ye. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
