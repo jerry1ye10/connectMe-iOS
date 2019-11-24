090fb5b66df0722aa1f9582de9c4ad9a75e4f61a
jerry1ye10 <jerry1ye10@gmail.com>
2019-11-24 13:07:11 -0500

    added pods + initial code

--- ConnectMe/ViewController.swift
+++ ConnectMe/ViewController.swift
@@ -11,11 +11,36 @@ import Firebase
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var createUser: UIButton!
+    @IBOutlet weak var registerPass: UITextField!
+    @IBOutlet weak var registerUser: UITextField!
+    @IBOutlet weak var loginPass: UITextField!
+    @IBOutlet weak var loginUser: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let ref = Database.database().reference()
+        ref.childByAutoId().setValue("Hello world")
     }
 
-
+    @IBAction func createUser(_ sender: Any) {
+        Auth.auth().createUser(withEmail: registerUser.text!, password: registerPass.text!) { authResult, error in
+            if error == nil {
+                print("Sucessfully loged in!")
+            }
+            else {
+                
+                print("Cannot log in the user")
+            }
+        }
+    }
+    
+    @IBAction func loginUser(_ sender: Any) {
+        Auth.auth().signIn(withEmail: "jerry1ye10@gmail.com", password: "abcdef") { [weak self] authResult, error in
+            guard let strongSelf = self else { return }
+            // ...
+        }
+    }
 }
 
--- Podfile
+++ Podfile
@@ -9,6 +9,7 @@ target 'ConnectMe' do
 
   pod 'Firebase/Core'
   pod 'Firebase/Database'
+  pod 'Firebase/Auth'
 
   target 'ConnectMeTests' do
     inherit! :search_paths
